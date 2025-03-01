30e8d37fc48aa62e4218bfbe30bb64ffae3913e7
LATEAM99 <egonzl92@outlook.com>
2025-02-28 23:02:35 -0800

    repository 1

---
+++ app/src/main/java/com/example/github/data/repository/PullRequestRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.example.github.data.repository
+
+import com.example.github.data.api.GithubService
+import javax.inject.Inject
+
+class PullRequestRepositoryImpl @Inject constructor(private val api: GithubService) {
+
+}
